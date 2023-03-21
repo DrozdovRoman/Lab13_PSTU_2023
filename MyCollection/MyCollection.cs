1cd8559fb5f3c8407a9ac0caff3813d9a6519161
Roman Drozdov <romandrozdov@icloud.com>
2023-03-21 14:58:47 +0500

    Create class Node.

---
+++ MyCollection/MyCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace MyCollection
+{
+    public class MyCollection<T> : IList<T> where T: ICloneable
+    {
+        
+    }
+}
